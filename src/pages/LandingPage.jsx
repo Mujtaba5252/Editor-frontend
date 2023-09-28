89f9eb3771cb278bd101fd1a82b9806d9a6203d4
muhammadyasirabbas <145529970+MujtabaInfini8ai@users.noreply.github.com>
2023-09-28 16:40:52 +0500

    again change

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,20 +5,20 @@ import { v4 as uuidV4 } from "uuid";
 const LandingPage = () => {
   console.log("Hello");
   const navigate = useNavigate();
-  // useEffect(() => {
-  //   navigate(`/documents/${uuidV4()}`);
-  //   return () => {};
-  // }, []);
+  useEffect(() => {
+    navigate(`/documents/${uuidV4()}`);
+    return () => {};
+  }, []);
   return (
     <div>
-      <Button
+      {/* <Button
         variant="contained"
         onClick={() => {
           navigate(`/documents/${uuidV4()}`);
         }}
       >
         Hello
-      </Button>
+      </Button> */}
     </div>
   );
 };
