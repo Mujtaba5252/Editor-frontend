27b4d49da585f017f543f1332f8b85005099de8d
muhammadyasirabbas <145529970+MujtabaInfini8ai@users.noreply.github.com>
2023-09-28 16:11:13 +0500

    landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,22 +1,24 @@
+import { Button } from "@mui/material";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { v4 as uuidV4 } from "uuid";
 const LandingPage = () => {
   console.log("Hello");
   const navigate = useNavigate();
-  useEffect(() => {
-    navigate(`/documents/${uuidV4()}`);
-    return () => {};
-  }, []);
+  // useEffect(() => {
+  //   navigate(`/documents/${uuidV4()}`);
+  //   return () => {};
+  // }, []);
   return (
     <div>
-      {/* <button
+      <Button
+        variant="contained"
         onClick={() => {
           navigate(`/documents/${uuidV4()}`);
         }}
       >
         Hello
-      </button> */}
+      </Button>
     </div>
   );
 };
