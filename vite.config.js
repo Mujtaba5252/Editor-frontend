ff43c507ea3a674117aa75919aa5122b998f3f9a
muhammadyasirabbas <145529970+MujtabaInfini8ai@users.noreply.github.com>
2023-10-02 10:14:43 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  css: {
+    preprocessorOptions: {
+      // Add any CSS-related options here if needed
+    },
+    include: ["path/to/react-quill/dist/quill.snow.css"],
+  },
+});
